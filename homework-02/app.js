5f87003293722d291c2b46c2553f14c19645289d
Maciej Kociemski <maciejkociemski@MacBook-Pro-Maciej.local>
2023-08-07 11:57:26 +0200

    feat: code rewritten

--- homework-02/app.js
+++ homework-02/app.js
@@ -1,41 +1,27 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const logger = require("morgan");
+const cors = require('cors');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const formatLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger('dev'));
+app.use(logger(formatLogger));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use("/api/contacts", contactsRouter);
+app.use((_, res) => {
+  res.status(404).json({ status: 404, message: "Not Found" });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((err, _, res, __) => {
+  res.status(err.status ?? 500).json({
+    status: err.status ?? 500,
+    message: err.message ?? "Internal Server Error",
+  });
 });
 
 module.exports = app;
